fe41473288ffca2398c088945df2a524e47f12ae
tyagi894ashish <tyagi894ashish@gmail.com>
2025-07-22 09:18:38 +0530

    main.tf code added

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_resource_group" "coltrg" {
+  for_each = var.coltmaprg
+  name     = each.key
+  location = each.value
+}
+
+resource "azurerm_storage_account" "coltstg" {
+  for_each                 = var.mapstg
+  name                     = each.value.name
+  resource_group_name      = each.value.RGname
+  location                 = each.value.location
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+depends_on = [ azurerm_resource_group.coltrg ]
+}
